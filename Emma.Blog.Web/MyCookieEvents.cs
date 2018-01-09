7090d4ae8b39f2d33af0c19c46294e49c9a2f1b6
songlin <526457385@qq.com>
2018-01-09 21:42:06 +0800

    删除多余的文件

--- Emma.Blog.Web/MyCookieEvents.cs
+++ Emma.Blog.Web/MyCookieEvents.cs
@@ -8,26 +8,11 @@ namespace Emma.Blog.Web
 {
     public class MyCookieEvents: CookieAuthenticationEvents
     {
-        public override Task SignedIn(CookieSignedInContext context)
-        {
-            base.SignedIn(context);
-            var claims = context.Principal.Claims;
-         
-            context.Response.Cookies.Append("token", "123");
-
-            return Task.CompletedTask ;
-        }
-
-        public override Task SigningIn(CookieSigningInContext context)
-        {
-            base.SigningIn(context);
-            var claims = context.HttpContext.User.Claims;
-            return Task.CompletedTask;
-        }
 
         public override Task ValidatePrincipal(CookieValidatePrincipalContext context)
         {
-            return Task.CompletedTask;
+            var claims = context.HttpContext.User.Claims;
+            return base.ValidatePrincipal(context);
         }
     }
 }
