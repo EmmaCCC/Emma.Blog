01b9a3558ca050c741cf23d8a714e8211ab207ef
Emma <526457385@qq.com>
2018-04-25 11:01:31 +0800

    测试问题稍作修改

--- Emma.Blog.Web/wwwroot/js/common.js
+++ Emma.Blog.Web/wwwroot/js/common.js
@@ -1,5 +1,5 @@
-﻿//var api_domain = "http://localhost:21222";
-var api_domain = "http://api.songlin.net.cn"
+﻿var api_domain = "http://localhost:21222";
+//var api_domain = "http://api.songlin.net.cn"
 $.ajaxSetup({
     dataType: 'json',
     xhrFields: {
